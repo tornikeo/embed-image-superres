bcd037e3f8dc06adf59d6859cbd3afa2b5cdcb33
tornikeo <tornikeonoprishvili@gmail.com>
2022-04-14 01:48:08 +0400

    Add rudimentary tfjs model loading

---
+++ static/js/main.js
@@ -0,0 +1,73 @@
+let model = null;
+
+async function getImage(img) {
+    // let img = document.getElementById(id)
+    
+    img = tf.cast(tf.browser.fromPixels(img), 'float32');
+
+    const offset = tf.scalar(255.0);
+    // Normalize the image from [0, 255] to [0, 1].
+    img = img.div(offset)
+    return img
+}
+
+
+async function stylize(content_elem, style_elem, result_elem) {
+    try {
+        // console.log(tf.getBackend());
+        const content = await getImage(content_elem)
+        const style = await getImage(style_elem)
+        const iters = tf.scalar(
+            // document.getElementById('iters').value, 
+            1,
+            dtype='int32')
+        const max_resolution = tf.scalar(
+            // document.getElementById('max_resolution').value,
+            256,
+            dtype='int32')
+        
+        console.log(content, style);
+        console.log('Images loaded successfully!');
+
+        const model = await loadModel();
+
+        console.log('Model loaded successfully!');
+        
+        console.log(model);
+        console.log('Doing stylization...');
+        const result = await model.executeAsync(
+            {content,style,iters,max_resolution},
+            ["result"]
+        ); 
+        console.log(result);
+        await tf.browser.toPixels(result, result_elem);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+async function load_tfjs_model() {
+    console.log("Loading model...");
+    model = await tf.loadGraphModel('static/js/model/model.json');
+    console.log("Model loaded.");
+    console.log('Warming up model...');
+    console.log(model);
+    await model.executeAsync(
+        {
+            'image_tensor_uint8':tf.ones([32,32,3], dtype ='int32'),
+        },
+    ); 
+    console.log('Model warmed up.');
+}
+
+window.addEventListener("load", e => {
+    console.log("window loaded");
+    load_tfjs_model();
+});
+
+document.getElementById('submit').addEventListener('click', 
+    e => {
+        console.log("submit clicked");
+    }
+);
+
